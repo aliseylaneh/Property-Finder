0a086cfea0e61a8f0c3be9ad9c5569bd237cef6d
aliseylaneh <aliseylaneh@gmail.com>
2025-01-11 09:09:21 +0330

    Docstring for AgentDjangoRepository

--- src/property_finder/repositories/django/agent.py
+++ src/property_finder/repositories/django/agent.py
@@ -11,35 +11,61 @@ from src.property_finder.models.exceptions.agent import AgentNotFound
 class AgentDjangoRepository:
 
     def all(self) -> QuerySet[Agent]:
+        """
+        Returns all agents from the database.
+        :return: QuerySet[Agent] of Agent instances.
+        """
         queryset = Agent.objects.prefetch_related("agent").all()
         return queryset
 
     def check_agent_exists(self, pk: int | None):
+        """
+        Check if an agent exists without returning any value.
+        :param pk: Agent ID to check or None.
+        """
         if pk:
             instance = Agent.objects.filter(pk=pk).first()
             if not instance:
                 raise AgentNotFound()
-            return instance
 
     def find_by_id(self, pk: int) -> Agent:
+        """
+        Retrieve an agent by its dedicated ID.
+        :param pk: Agent ID which must be Integer.
+        :return: The retrieved instance of Agent.
+        """
         instance = Agent.objects.filter(pk=pk).first()
         if not instance:
             raise AgentNotFound()
         return instance
 
     def create(self, name: str, email: str, phone_number: str) -> Agent:
+        """
+        Creates a new agent with below params:
+        :param name: The name of the agent which is string.
+        :param email: The email address of the agent which is string.
+        :param phone_number: The phone number of the agent which is string.
+        :return: The created instance of Agent.
+        """
         with transaction.atomic():
             instance = Agent.objects.create(name=name, email=email, phone_number=phone_number)
             return instance
 
     def delete(self, pk: int):
+        """
+        Deletes an agent by its dedicated ID.
+        :param pk: Agent ID to delete.
+        """
         with transaction.atomic():
             Agent.objects.filter(pk=pk).delete()
 
     def update(self, pk: int, updates: Dict[str, Any]) -> Agent:
+        """
+        Update an agent by a given dictionary of fields and their values
+        :param pk: Agent ID to update.
+        :param updates: Dictionary of Agent fields and their new values.
+        """
         with transaction.atomic():
             instance = self.find_by_id(pk=pk)
-            instance, is_updated = InstanceUpdateService(instance=instance,
-                                                         data=updates,
-                                                         fields=list(updates.keys()))
+            instance, is_updated = InstanceUpdateService(instance=instance, data=updates, fields=list(updates.keys()))
             return instance
